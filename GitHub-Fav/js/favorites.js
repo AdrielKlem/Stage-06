b1e009b8502688f8471e5c2de85d00c0317d67e1
Adriel Klem <adrielklem16@gmail.com>
2023-08-11 16:40:01 -0300

    update css

--- GitHub-Fav/js/favorites.js
+++ GitHub-Fav/js/favorites.js
@@ -3,6 +3,22 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = [{
+            login: 'maykbrito',
+            name: "Mayk Brito",
+            public_repos: '76',
+            followers: '12000'
+        }
+        ,{
+            login: 'diego3g',
+            name: "Diego Fernandes",
+            public_repos: '78',
+            followers: '12001'
+        }]
     }
 }
 
@@ -11,19 +27,53 @@ export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
 
+        this.tbody = this.root.querySelector("table tbody")
+
         this.update()
     }
 
     update() {
         this.removeAllTr()
+      
+        this.entries.forEach( user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `imagem de ${user.name}`
+            row.querySelector(".user p").textContent = user.name
+            row.querySelector(".user span").textContent = user.login
+            row.querySelector(".repositories").textContent = user.public_repos
+            row.querySelector(".followers").textContent = user.followers
+
+            this.tbody.append(row)
+        })
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector("table tbody")
-
-        tbody.querySelectorAll('tr')
+        this.tbody.querySelectorAll('tr')
         .forEach(tr => {
           tr.remove()      
         })
+    }
+
+
+    createRow() {
+        const tr = document.createElement("tr")
+
+        tr.innerHTML = `
+            <td class="user">
+              <img
+                src="https://github.com/maykbrito.png"
+                alt="Imagem de maykbrito"
+              />
+              <a href="https://github.com/maykbrito"></a>
+              <p>Mayk Brito</p>
+              <span>maykbrito</span>
+            </td>
+            <td class="repositories">76</td>
+            <td class="followers">9598</td>
+            <td><button class="remove">&times;</button></td>
+            `
+        return tr
     }
 }
