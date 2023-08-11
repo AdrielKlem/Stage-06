eb7bb6ef3b722756bcf4c574547b63c67160f458
Adriel Klem <adrielklem16@gmail.com>
2023-08-11 10:10:41 -0300

    update

--- SPA/js/index.js
+++ SPA/js/index.js
@@ -5,8 +5,14 @@ const router = new Router()
 router.add("/", "/pages/home.html")
 router.add("/about", "/pages/about.html")
 router.add("/contact", "/pages/contact.html")
-router.add(404, "/pages/404.html")
+router.add("/404", "/pages/404.html")
 
 router.handle();
 window.onpopstate = () => router.handle();
-window.route = () => router.route()
+window.route = () => router.route() /*
+
+const links = document.querySelectorAll("a");
+links.forEach(link => {
+  link.addEventListener("click", router.route.bind(router));
+});
+*/
--- SPA/js/router.js
+++ SPA/js/router.js
@@ -16,11 +16,9 @@ export class Router {
 
     handle() {
         const { pathname } = window.location;
-        const route = routes[pathname] || routes[404];
+        const route = this.routes[pathname] || this.routes["/404"];
 
-        fetch(route)
-        .then((data) => data.text())
-        .then((html) => {
+        fetch(route).then((data) => data.text()).then((html) => {
             document.querySelector("#app").innerHTML = html;
         });
     }
